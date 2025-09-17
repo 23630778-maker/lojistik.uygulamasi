e941eb6bb5d13865b60e6da43a7cea705696d52b
Nisa KARAMAN <23630778@stu.omu.edu.tr>
2025-09-17 07:40:37 +0300

    Add backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,8 @@ app.secret_key = "supersecretkey"
 
 # Excel dosyalarının yolları
 # Excel dosyaları
-EXCEL_FILE_LOCAL = "C:/Users/nisak/OneDrive/lojistik.xlsx" 
-EXCEL_FILE_ONEDRIVE = "C:/Users/nisak/OneDrive/lojistik.xlsx"
+EXCEL_FILE_LOCAL = r"C:/Users/nisak/OneDrive/lojistik.xlsx" 
+EXCEL_FILE_ONEDRIVE = r"C:/Users/nisak/OneDrive/lojistik.xlsx"
 
 
 # Google Drive Excel file ID
